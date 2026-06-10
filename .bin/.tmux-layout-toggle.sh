041cb1c9b9880568ce2dd6200846b3c7763d9207
cazuu <16042740+cazuu@users.noreply.github.com>
2026-06-10 18:10:47 +0900

    Preserve window names through layout toggle and disable automatic-rename

--- .bin/.tmux-layout-toggle.sh
+++ .bin/.tmux-layout-toggle.sh
@@ -10,6 +10,11 @@ window_count=$(tmux display-message -p '#{session_windows}')
 pane_count=$(tmux display-message -p '#{window_panes}')
 
 if [ "$window_count" -gt 1 ]; then
+    # Tag each pane with its window name so the name survives the join/break roundtrip
+    while IFS=$'\t' read -r win_id pane_id win_name; do
+        [[ "$win_id" == "$current_window" ]] && continue
+        tmux select-pane -t "$pane_id" -T "$win_name"
+    done < <(tmux list-panes -s -F '#{window_id}	#{pane_id}	#{window_name}')
     for p in $(tmux list-panes -s -F '#{window_id} #{pane_id}' \
                | awk -v cur="$current_window" '$1 != cur { print $2 }'); do
         tmux join-pane -h -s "$p" -t "$current_window"
@@ -17,10 +22,10 @@ if [ "$window_count" -gt 1 ]; then
     tmux select-layout -t "$current_window" even-horizontal
     tmux display-message "merged into 1 window"
 elif [ "$pane_count" -gt 1 ]; then
-    for p in $(tmux list-panes -t "$current_window" -F '#{pane_id}' \
-               | grep -v "^${current_pane}$"); do
-        tmux break-pane -d -s "$p"
-    done
+    while IFS=$'\t' read -r pane_id pane_title; do
+        tmux break-pane -d -s "$pane_id" -n "$pane_title"
+    done < <(tmux list-panes -t "$current_window" -F '#{pane_id}	#{pane_title}' \
+               | grep -v "^${current_pane}	")
     tmux display-message "split into separate windows"
 else
     tmux display-message "nothing to toggle"
